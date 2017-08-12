fdbe8a2db7c6a531877110d783fda189cce96fac
zhiqin-zbb <451053802@qq.com>
2017-08-12 13:39:31 +0800

    <mvc:annotation-driven/>

--- src/main/java/com/zhiqin/SpringMVC/controller/UserController.java
+++ src/main/java/com/zhiqin/SpringMVC/controller/UserController.java
@@ -15,45 +15,48 @@ import com.zhiqin.SpringMVC.service.UserService;
 @Controller
 @RequestMapping("/")
 public class UserController {
-	@Autowired
-	UserService userService;
+    @Autowired
+    UserService userService;
 
-	@RequestMapping("index")
-	public String index() {
-		return "index";
-	}
+    @RequestMapping("index")
+    public String index() {
+        return "index";
+    }
 
-	// @RequestMapping("login")
-	// public String login(@RequestParam("username") String username,
-	// @RequestParam("password") String password,
-	// ModelMap modelMap) {
-	// if (userService.loginVerify(username, password)) {
-	// User user = userService.getUserByUsername(username);
-	// modelMap.addAttribute("user", user);
-	// return "/user/showInfo";
-	// } else
-	// return "fail";
-	// }
+//    @RequestMapping("login")
+//    public String login(@RequestParam("username") String username,
+//                        @RequestParam("password") String password,
+//                        ModelMap modelMap) {
+//        if (userService.loginVerify(username, password)) {
+//            User user = userService.getUserByUsername(username);
+//            modelMap.addAttribute("user", user);
+//            return "/user/showInfo";
+//        } else
+//            return "fail";
+//    }
 
-	@RequestMapping("getUserInfoById")
-	public @ResponseBody Map getUserInfoById(@RequestParam("id") int id) {
-		Map<String, User> map = new HashMap<String, User>();
-		map.put("userInfo", userService.getUserById(id));
-		return map;
-	}
+    @RequestMapping("getUserInfoById")
+    public @ResponseBody
+    Map getUserInfoById(@RequestParam("id") int id) {
+        Map<String, User> map = new HashMap<>();
+        map.put("userInfo", userService.getUserById(id));
+        return map;
+    }
 
-	@RequestMapping("getUserInfo")
-	public @ResponseBody Map getUserInfo(@RequestParam("username") String username) {
-		Map<String, User> map = new HashMap<String, User>();
-		map.put("userInfo", userService.getUserByUsername(username));
-		return map;
-	}
+    @RequestMapping("getUserInfo")
+    public @ResponseBody
+    Map getUserInfo(@RequestParam("username") String username) {
+        Map<String, User> map = new HashMap<>();
+        map.put("userInfo", userService.getUserByUsername(username));
+        return map;
+    }
 
-	@RequestMapping("verify")
-	public @ResponseBody Map verify(@RequestParam("username") String username,
-			@RequestParam("password") String password) {
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("result", userService.loginVerify(username, password));
-		return map;
-	}
+    @RequestMapping("verify")
+    public @ResponseBody
+    Map verify(@RequestParam("username") String username,
+               @RequestParam("password") String password) {
+        Map<String, String> map = new HashMap<>();
+        map.put("result", userService.loginVerify(username, password));
+        return map;
+    }
 }
